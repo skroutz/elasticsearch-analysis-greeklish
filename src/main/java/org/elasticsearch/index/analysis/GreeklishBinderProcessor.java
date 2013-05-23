2733a6510af58118c7885e29c878de4a5950fcdf
Tasos Stathopoulos <stathopa@skroutz.gr>
2013-05-23 14:11:00 +0300

    Minor reformat of the code

--- src/main/java/org/elasticsearch/index/analysis/GreeklishBinderProcessor.java
+++ src/main/java/org/elasticsearch/index/analysis/GreeklishBinderProcessor.java
@@ -2,7 +2,8 @@ package org.elasticsearch.index.analysis;
 
 public class GreeklishBinderProcessor extends AnalysisModule.AnalysisBinderProcessor {
 	
-	@Override public void processTokenFilters(TokenFiltersBindings tokenFiltersBindings) {
+	@Override
+	public void processTokenFilters(TokenFiltersBindings tokenFiltersBindings) {
 		tokenFiltersBindings.processTokenFilter("greeklish", GreeklishTokenFilterFactory.class);
 	}
 
