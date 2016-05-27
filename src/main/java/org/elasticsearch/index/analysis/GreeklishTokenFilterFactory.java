773ecc4ccb7a4701be84bad433f0b67f0a9b6d53
Panagiotis Nikitopoulos <panosbobos@yahoo.gr>
2016-05-27 14:41:50 +0300

    Added support for elasticsearch 2.3.3

--- src/main/java/org/elasticsearch/index/analysis/GreeklishTokenFilterFactory.java
+++ src/main/java/org/elasticsearch/index/analysis/GreeklishTokenFilterFactory.java
@@ -5,15 +5,15 @@ import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.assistedinject.Assisted;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.index.Index;
-import org.elasticsearch.index.settings.IndexSettings;
+import org.elasticsearch.index.settings.IndexSettingsService;
 
 public class GreeklishTokenFilterFactory  extends AbstractTokenFilterFactory {
 	private final int maxExpansions;
 	private final boolean generateGreekVariants;
 
 	@Inject
-	public GreeklishTokenFilterFactory(Index index, @IndexSettings Settings indexSettings, @Assisted String name, @Assisted Settings settings) {
-        super(index, indexSettings, name, settings);
+	public GreeklishTokenFilterFactory(Index index, IndexSettingsService indexSettingsService, @Assisted String name, @Assisted Settings settings) {
+        super(index, indexSettingsService.indexSettings(), name, settings);
 
         this.maxExpansions = settings.getAsInt("max_expansions", 20);
         this.generateGreekVariants = settings.getAsBoolean("greek_variants", true);
