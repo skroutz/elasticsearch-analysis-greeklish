3dafb5483479ae19a1da6fdf2b0acde9048e2f96
Michalis Despotopoulos <mdespoto@skroutz.gr>
2017-04-18 18:06:52 +0300

    Simplify the analysis test by leveraging the elastic test framework
    https://www.elastic.co/guide/en/elasticsearch/reference/current/using-elasticsearch-test-classes.html
    https://github.com/elastic/elasticsearch/blob/v5.1.2/test/framework/src/main/java/org/elasticsearch/test/ESTestCase.java

--- src/test/java/org/elasticsearch/index/analysis/SimpleGreeklishAnalysisTest.java
+++ src/test/java/org/elasticsearch/index/analysis/SimpleGreeklishAnalysisTest.java
@@ -1,51 +1,22 @@
 package org.elasticsearch.index.analysis;
 
-import org.hamcrest.MatcherAssert;
-import org.testng.annotations.Test;
-import org.elasticsearch.Version;
-import org.elasticsearch.cluster.metadata.IndexMetaData;
-import org.elasticsearch.common.inject.Injector;
-import org.elasticsearch.common.inject.ModulesBuilder;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.settings.SettingsModule;
-import org.elasticsearch.env.Environment;
-import org.elasticsearch.env.EnvironmentModule;
 import org.elasticsearch.index.Index;
-import org.elasticsearch.index.IndexNameModule;
-import org.elasticsearch.index.settings.IndexSettingsModule;
-import org.elasticsearch.indices.analysis.IndicesAnalysisService;
 
-import static org.elasticsearch.common.settings.Settings.settingsBuilder;
+import org.elasticsearch.test.ESTestCase;
+import org.elasticsearch.plugin.analysis.greeklish.GreeklishPlugin;
+
+import java.io.IOException;
+
 import static org.hamcrest.Matchers.instanceOf;
 
-public class SimpleGreeklishAnalysisTest {
+class SimpleSkroutzGreekStemmerAnalysisTest extends ESTestCase {
+    public void testSkroutzGreekStemmerAnalysis() throws IOException {
+        TestAnalysis analysis = createTestAnalysis(new Index("test", "_na_"),
+                Settings.EMPTY, new GreeklishPlugin());
 
-    @Test
-    public void testGreeklishAnalysis() {
-        Index index = new Index("test");
-
-        Settings indexSettings = settingsBuilder()
-                .put(IndexMetaData.SETTING_VERSION_CREATED, Version.CURRENT)
-                .put("path.home", "/")
-                .build();
-
-        Injector parentInjector = new ModulesBuilder()
-            .add(new SettingsModule(indexSettings),
-                 new EnvironmentModule(new Environment(indexSettings)))
-            .createInjector();
-
-        Injector injector = new ModulesBuilder()
-            .add(new IndexSettingsModule(index, indexSettings),
-                 new IndexNameModule(index),
-                 new AnalysisModule(indexSettings,
-                                    parentInjector.getInstance(IndicesAnalysisService.class))
-                 .addProcessor(new GreeklishBinderProcessor()))
-            .createChildInjector(parentInjector);
-
-        AnalysisService analysisService = injector.getInstance(AnalysisService.class);
-
-        TokenFilterFactory filterFactory = analysisService.tokenFilter("greeklish");
-        MatcherAssert.assertThat(filterFactory, instanceOf(GreeklishTokenFilterFactory.class));
+        TokenFilterFactory filterFactory = analysis.tokenFilter.get
+                ("skroutz_greeklish");
+        assertThat(filterFactory, instanceOf(GreeklishTokenFilterFactory.class));
     }
-
 }
